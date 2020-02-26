1e93448ff216034da923e6b0e0534cb3dc5fe365
Kyle Talmage <ktalmage85@gmail.com>
2020-02-26 23:16:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,6 +23,7 @@ class Transfer
     return "Transaction rejected. Please check your account balance."
     end
     end
+  end
     
     
     def reverse_transfer
@@ -33,5 +34,6 @@ class Transfer
     end
   end
 end
+
 
 
