b603ccc8996891d7e5240c0b1c31618b0a7e8523
Kyle Talmage <ktalmage85@gmail.com>
2020-02-26 23:22:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,7 +21,7 @@ class Transfer
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    end
+      end
     end
   
     def reverse_transfer
@@ -29,7 +29,7 @@ class Transfer
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
-    
+    end
   end
 end
 
